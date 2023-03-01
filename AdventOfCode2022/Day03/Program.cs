37096f419047b20e97374b200020b87a5cca6054
Nico Auerswald <auersw.nico@gmail.com>
2023-03-01 19:36:47 +0100

    Day03 part one

---
+++ AdventOfCode2022/Day03/Program.cs
@@ -0,0 +1,28 @@
+﻿const string INPUT_FOLDER = "input";
+const string INPUT_FILE = "input.txt";
+string path = Path.Combine(INPUT_FOLDER, INPUT_FILE);
+
+IEnumerable<string> lines = File.ReadLines(path);
+var linesArray = lines as string[] ?? lines.ToArray();
+
+int prioritySum = 0;
+
+const string priorityString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+foreach (var str in linesArray)
+{
+   int halfLength = str.Length / 2;
+
+   string firstHalf = str[..halfLength];
+   string secondHalf = str[halfLength..];
+
+   IEnumerable<char> duplicates = firstHalf.Intersect(secondHalf);
+
+   foreach (var duplicate in duplicates)
+   {
+      prioritySum += (priorityString.IndexOf(duplicate) + 1);
+   }
+}
+
+Console.WriteLine(prioritySum);
+Console.WriteLine(priorityString.IndexOf("Z"));
